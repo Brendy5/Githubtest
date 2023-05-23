30055c49f3e778e0c30263b70cd2a1a03dd855c4
Ogwe Chibuike Brendan <ogwebrendan@gmail.com>
2023-05-22 13:44:51 -1100

    quick update

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Date and Time</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+
+    <body>
+        <?php
+            date_default_timezone_set("Africa/Lagos");
+            echo date("F h, Y h:i:s A");
+            echo ' WAT';
+        ?> 
+    </body>
+</html>
